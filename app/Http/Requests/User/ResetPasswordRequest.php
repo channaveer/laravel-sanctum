6f66dac464a60af9b5e55de5b86ce8f9591b99bf
Channaveer Hakari <channaveer888@gmail.com>
2021-08-30 01:43:54 +0530

    fixed the request to point to my custom request

--- app/Http/Requests/User/ResetPasswordRequest.php
+++ app/Http/Requests/User/ResetPasswordRequest.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Requests\User;
 
-use Illuminate\Foundation\Http\FormRequest;
+use App\Http\Requests\FormRequest;
 
 class ResetPasswordRequest extends FormRequest
 {
--- app/Http/Requests/User/VerifyResetPasswordRequest.php
+++ app/Http/Requests/User/VerifyResetPasswordRequest.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Requests\User;
 
-use Illuminate\Foundation\Http\FormRequest;
+use App\Http\Requests\FormRequest;
 
 class VerifyResetPasswordRequest extends FormRequest
 {
